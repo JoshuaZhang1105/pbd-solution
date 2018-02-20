dc94736c87bf397abc1096eafe57933608b42a96
JoshuaZhang1105 <36411090+JoshuaZhang1105@users.noreply.github.com>
2018-02-20 09:03:47 -0500

    12

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables
+{
+	public static void main( String[] args )
+	{
+		String Name = "Josh";
+		int Year = 2333;
+		System.out.println("My name is " + Name + " and I will graduate in " + Year + ".");
+	}
+}
+		
