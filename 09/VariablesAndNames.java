80d1156b9181b162a3877eb684bd1392d0a2e5c8
JoshuaZhang1105 <36411090+JoshuaZhang1105@users.noreply.github.com>
2018-02-20 08:42:27 -0500

    09

---
+++ 09/VariablesAndNames.java
@@ -0,0 +1,14 @@
+public class VariablesAndNames
+{
+    public static void main(String[] args)
+    {
+		int Students, Teacher, Candy, AverageStudentCanGet;
+		Students = 30;
+		Candy = 60;
+		AverageStudentCanGet = Students / Candy;
+		
+		System.out.println("There are " + Students + "students in the class");
+		System.out.println("There are " + Candy + "candies in Mr.Gallo's hand.");
+		System.out.println("Each student can get " + AverageStudentCanGet + "candies from Mr.Gallo.");
+	}
+}
